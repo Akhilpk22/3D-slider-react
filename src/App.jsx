8aa875522c23a15f5195fe0f651dd34b06de7a49
Ak-Akhilpk <akhilpkweb201@gmail.com>
2023-12-25 14:02:50 +0530

    react app completed

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Slider from "./pages/Slider";
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div className="container">
+      <Slider />
+  
+    </div>
+  );
+}
+
+export default App;
